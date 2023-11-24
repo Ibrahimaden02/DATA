5d636f03aed8839c765b9b3961838693b20f6757
Ibrahimaden02 <133215513+Ibrahimaden02@users.noreply.github.com>
2023-11-24 13:25:10 +0100

    Add files via upload

---
+++ post.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>POST Formulier</title>
+
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+            margin: 20px;
+        }
+
+        h1 {
+            color: #333;
+        }
+
+        form {
+            max-width: 400px;
+            margin: 20px auto;
+            padding: 15px;
+            background-color: #fff;
+            border-radius: 5px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+
+        label {
+            display: block;
+            margin: 10px 0 5px;
+        }
+
+        input {
+            width: 100%;
+            padding: 8px;
+            margin-bottom: 10px;
+            box-sizing: border-box;
+        }
+
+        input[type="submit"] {
+            background-color: #4caf50;
+            color: #fff;
+            cursor: pointer;
+        }
+
+        input[type="submit"]:hover {
+            background-color: #45a049;
+        }
+
+        h2 {
+            color: #333;
+            margin-top: 20px;
+        }
+
+        p {
+            margin-bottom: 5px;
+        }
+    </style>
+</head>
+<body>
+    <h1>Vul het formulier</h1>
+    <form action="" method="POST">
+        <label for="naam">Naam:</label>
+        <input type="text" name="naam" required><br>
+
+        <label for="achternaam">Achternaam:</label>
+        <input type="text" name="achternaam" required><br>
+
+        <label for="leeftijd">Leeftijd:</label>
+        <input type="number" name="leeftijd" required><br>
+
+        <label for="adres">Adres:</label>
+        <input type="text" name="adres" required><br>
+
+        <label for="email">Email:</label>
+        <input type="email" name="email" required><br>
+
+        <input type="submit" value="Verstuur">
+    </form>
+
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST" && !empty($_POST)) {
+        echo "<h2>Ingevoerde gegevens:</h2>";
+        echo "<p>Naam: " . $_POST['naam'] . "</p>";
+        echo "<p>Achternaam: " . $_POST['achternaam'] . "</p>";
+        echo "<p>Leeftijd: " . $_POST['leeftijd'] . "</p>";
+        echo "<p>Adres: " . $_POST['adres'] . "</p>";
+        echo "<p>Email: " . $_POST['email'] . "</p>";
+    }
+    ?>
+</body>
+</html>
